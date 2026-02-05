5655a572a214c81c982bd2f49357f29033f0ed07
derick1129 <derickjohn1129@gmail.com>
2026-02-05 22:12:11 +0530

    created auth routes

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,27 @@
+import type { Request, Response } from "express";
+import { registerUser, loginUser } from "./auth.service";
+
+export const register = async (req: Request, res: Response) => {
+    try {
+        const user = await registerUser(
+            req.body.name,
+            req.body.email,
+            req.body.password
+        );
+        res.status(201).json(user);
+    } catch (err: any) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+export const login = async (req:Request, res: Response) => {
+    try {
+        const token = await loginUser(
+            req.body.email,
+            req.body.password
+        );
+        res.json({ token });
+    } catch (err: any) {
+        res.status(401).json({ message: err.message });
+    }
+};
---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,38 @@
+import User from "./user.model";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+export const registerUser = async (
+    name: string,
+    email: string,
+    password: string
+) => {
+    const hashed = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+        name,
+        email,
+        password: hashed
+    });
+
+    return user;
+}
+
+export const loginUser = async (
+    email: string,
+    password: string
+) => {
+    const user = await User.findOne({ email });
+    if (!user) throw new Error("User not found");
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) throw new Error("Invalid credentials");
+
+    const token = jwt.sign(
+        { id: user._id, role: user.role },
+        process.env.JWT_SECRET as string,
+        { expiresIn: "7d" }
+    );
+
+    return token;
+}
