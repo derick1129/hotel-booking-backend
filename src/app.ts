67225bc61e7f5acc233b8fbf44c1235b25afde6f
derick1129 <derickjohn1129@gmail.com>
2026-02-09 23:29:15 +0530

    added payment routes

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import authRoutes from "./modules/auth/auth.routes"
 import roomRoutes from "./modules/room/room.routes";
 import hotelRoutes from "./modules/hotel/hotel.routes"
 import bookingRoutes from "./modules/booking/booking.routes";
+import paymentRoutes from "./modules/payment/payment.routes";
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/api/payments", paymentRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/hotels", hotelRoutes);
 app.use("/api/rooms", roomRoutes);
